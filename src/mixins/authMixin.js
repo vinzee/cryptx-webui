0de67f096f0c4abe3d200ea98abff606c9148da3
Vineet Ahirkar <vinzee93@gmail.com>
2017-11-16 22:02:37 -0500

    handle after signin redirection

--- src/mixins/authMixin.js
+++ src/mixins/authMixin.js
@@ -5,52 +5,28 @@ Vue.mixin({
     login (email, password) {
       // this.$auth.login({ email, password }).then((res) => {
       //   if (res.success) {
+      /* eslint-disable object-property-newline  */
       let res = {
         session: 'vineet',
         user: {
-          firstName: 'Vineet',
-          lastName: 'Ahirkar',
-          email: 'vinzee93@gmail.com',
-          address: 'Maryland, US',
-          city: 'baltimore',
-          country: 'United States',
-          postalCode: '21227',
-          virtual_wallet: {
-            balance: 1234
-          },
-          bank_accounts: [{
-            id: '1',
-            name: 'Bank of America',
-            account_number: '1234',
-            type: 'credit'
-          }, {
-            id: '2',
-            name: 'PNC',
-            account_number: '6789',
-            type: 'debit'
-          }],
+          firstName: 'Vineet', lastName: 'Ahirkar', email: 'vinzee93@gmail.com',
+          address: 'Maryland, US', city: 'baltimore', country: 'United States', postalCode: '21227',
+          virtual_wallet: { balance: 1234 },
+          bank_accounts: [ { id: '1', name: 'Bank of America', account_number: '1234', type: 'credit' }, {id: '2', name: 'PNC', account_number: '6789', type: 'debit'} ],
           investments: {
-            bitcoin: {
-              amount: 1.7
-            },
-            litecoin: {
-              amount: 1.3
-            },
-            etherium: {
-              amount: 0.5
-            },
-            ripple: {
-              amount: 1.5
-            }
+            bitcoin: { amount: 1.7 }, litecoin: { amount: 1.3 }, etherium: { amount: 0.5 }, ripple: { amount: 1.5 }
           }
         }
       }
 
       console.log('login successful !')
       this.$cookie.set('session', res.session, {expires: '1H'}) // , domain: this.$config.baseURL
+      console.log(this.$route.query.redirect)
+
       this.$store.dispatch('setUserData', res.user).then(() => {
         this.$store.dispatch('login')
-        this.$router.push('/')
+        let redirectPath = this.$route.query.redirect == null ? '/' : this.$route.query.redirect
+        this.$router.push(redirectPath)
         this.$notify('User logged in sucessfully !', 'ti-bank')
       })
       //   } else {
