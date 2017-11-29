3ee666a466e6f012f1b8c3ec7362610627b93da2
Vineet Ahirkar <vinzee93@gmail.com>
2017-11-29 14:38:23 -0500

    environment based baseURL

--- src/vue.configs.js
+++ src/vue.configs.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import vueConfig from 'vue-config'
 
+console.log('process.env.NODE_ENV: ', process.env.NODE_ENV)
+
 const configs = {
-  BASE_URL: 'http://10.0.0.93:8080',
   LOGIN_URL: '/login',
   REGISTER_URL: '/register',
   momentFormat: 'MMMM Do YYYY, h:mm:ss a',
@@ -10,7 +11,12 @@ const configs = {
   CRYPTOCOMPARE_URL: 'https://www.cryptocompare.com',
   CRYPTOCOMPARE_API_URL: 'https://min-api.cryptocompare.com',
   COINMARKETCAP_API_URL: 'https://api.coinmarketcap.com'
+}
 
+if (process.env.NODE_ENV === 'production') {
+  configs.BASE_URL = 'http://169.55.144.76:8080'
+} else {
+  configs.BASE_URL = 'http://10.0.0.93:8080'
 }
 
 Vue.use(vueConfig, configs)
